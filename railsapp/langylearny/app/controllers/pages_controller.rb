6c38f58cd53f107d34b10f969a9c1e0486545d79
Mary <mary_bescuca@yahoo.com>
2015-01-08 00:03:34 +0200

    add some links and deal with navbar

--- railsapp/langylearny/app/controllers/pages_controller.rb
+++ railsapp/langylearny/app/controllers/pages_controller.rb
@@ -3,5 +3,9 @@ class PagesController < ApplicationController
 	def home
 	
 	end
+
+	def registration
+
+	end
 	
 end
--- railsapp/langylearny/config/routes.rb
+++ railsapp/langylearny/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 	
 	root 'pages#home'
+
+  get 'registration' => 'pages#registration'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
