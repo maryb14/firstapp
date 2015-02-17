7db1c3f5f9c7904ef6ecf6d28098195d86edbd7e
Dominika Mips <dominika.mips@amberbit.com>
2015-02-17 20:52:07 +0100

    fix current_user helper

--- railsapp/langylearny/app/controllers/application_controller.rb
+++ railsapp/langylearny/app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-  	@user ||= User.find_by(id: session[:user_id])
+  	@current_user ||= User.find_by(id: session[:user_id])
   end
 
 end
